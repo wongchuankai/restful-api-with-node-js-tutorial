977be52f4464bd3b3a358bbbbad9f2dd5681e5d7
CK <wongchuankai97@gmail.com>
2020-04-06 22:12:08 +0800

    no message

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+//only things starting with /products in url will be forwarded to
+//products.js
+
+app.use(morgan('dev'));
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500 );
+    res.json({
+        message : error.message
+    })
+});
+
+module.exports = app;
